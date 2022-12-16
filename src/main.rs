5d669f5d12862c1e5c7cc2c80a67e919a3d0f928
david-1st <chaparan.david@gmail.com>
2022-12-16 19:42:10 +0300

    basic functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
 fn main() {
-    println!("Hello, world!");
+    let mut notes = Vec::new();
+
+    print_menu();
+
+    loop {
+        let command = read_input();
+
+        match command.trim() {
+            "show" => show_notes(&notes),
+            "add" => add_notes(&mut notes),
+            _ => break,
+        }
+    }
+}
+
+fn print_menu() {
+    println!("Notes app");
+    println!("show - show all notes");
+    println!("add - add new note");
+}
+
+fn read_input() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).unwrap();
+    buffer
+}
+
+fn add_notes(notes: &mut Vec<String>) {
+    println!("-----");
+    println!("Enter note");
+
+    let input = read_input();
+
+    let note = input.trim().to_string();
+
+    notes.push(note);
+    println!("-----")
+}
+
+fn show_notes(verc: &Vec<String>) {
+    println!("-----");
+    for item in verc {
+        println!("{}", item);
+    }
+    println!("-----")
 }
